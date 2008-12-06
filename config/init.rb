6709ede22f340aa827ed870aba7be9cbe43e9d90
Ken Robertson <ken@invalidlogic.com>
2008-12-06 14:37:33 -0800

    Brand new merb app, hello Tigger

---
+++ config/init.rb
@@ -0,0 +1,24 @@
+# Go to http://wiki.merbivore.com/pages/init-rb
+ 
+require 'config/dependencies.rb'
+ 
+use_orm :datamapper
+use_test :rspec
+use_template_engine :erb
+ 
+Merb::Config.use do |c|
+  c[:use_mutex] = false
+  c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
+  
+  # cookie session store configuration
+  c[:session_secret_key]  = '6c97c270a6bd5b9250840d6920a0602451218fda'  # required for cookie session store
+  c[:session_id_key] = '_tigger_session_id' # cookie session id key, defaults to "_session_id"
+end
+ 
+Merb::BootLoader.before_app_loads do
+  # This will get executed after dependencies have been loaded but before your app's classes have loaded.
+end
+ 
+Merb::BootLoader.after_app_loads do
+  # This will get executed after your app's classes have been loaded.
+end
